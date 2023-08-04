2e9202d5556a7cbc3d0292e987fc44ce7bd56746
jonas-45 <jonaswullah@gmail.com>
2023-08-04 06:57:01 +0000

    Add routes for foods and recipe_foods to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
+  get 'recipe_foods/index'
+  get 'recipe_foods/show'
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/destroy'
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,8 +15,14 @@ Rails.application.routes.draw do
   get 'welcome/index', to: 'welcome#index', as: :welcome
   patch 'recipes/:id/update_public', to: 'recipes#update_public', as: 'recipe_update_public'
 
+  resources :foods
+
   resources :recipes, only: %i[index show new create destroy] do
     patch :update_public, on: :member
+  end
+
+  resources :recipes do
+    resources :shopping_lists, only: [:index]
   end
 
   resources :shopping_lists, only: [:index]
