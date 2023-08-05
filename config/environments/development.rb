6929dd19c18880f8a183b6127fc7e4c2a720cfd4
jonas-45 <jonaswullah@gmail.com>
2023-08-05 10:35:08 +0000

    Add integration test for clicking 'Add Food' link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Set the default host for Devise mailer URLs
+  config.action_mailer.default_url_options = { host: 'example.com' }
 end
